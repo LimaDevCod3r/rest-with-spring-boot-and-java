b534101371b5a521017aabdd830bf66136e83af2
Diego Lima <diegolima20100@gmail.com>
2025-04-28 17:37:22 -0300

    Definindo os Métodos de validação e Executando a Aplicação

--- src/main/java/br/LimaDevCod3r/Controllers/MathController.java
+++ src/main/java/br/LimaDevCod3r/Controllers/MathController.java
@@ -2,6 +2,7 @@ package br.LimaDevCod3r.Controllers;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
     // http://localhost:8080/math/sum/3/5
-    @RequestMapping("/sum/{numbOne}/{numbTwo}")
-    public Double sum(@PathVariable("numbOne") String numbOne, @PathVariable("numbTwo") String numbTwo){
-        return 1D;
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+    }
+
+    public static Double covertToDouble(String strNumber) {
+        if (strNumber == null) return 0d;
+        String number = strNumber.replaceAll(",", ".");// Moeda Americana x Brasileira
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
