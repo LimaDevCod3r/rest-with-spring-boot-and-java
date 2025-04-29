3b6b3c40bb36d29ac9254b7c62260c058e6cd664
Diego Lima <diegolima20100@gmail.com>
2025-04-29 10:19:50 -0300

    Adiciona endpoint findAll para retornar todas as pessoas mockadas

--- src/main/java/br/LimaDevCod3r/Controllers/PersonController.java
+++ src/main/java/br/LimaDevCod3r/Controllers/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -16,10 +18,17 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return this.service.findAll();
+    }
+
     @RequestMapping(value = "/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id) {
         return this.service.findById(id);
     }
+
+
 }
--- src/main/java/br/LimaDevCod3r/Services/PersonService.java
+++ src/main/java/br/LimaDevCod3r/Services/PersonService.java
@@ -3,16 +3,39 @@ package br.LimaDevCod3r.Services;
 import br.LimaDevCod3r.Model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
 public class PersonService {
-    private final AtomicLong couter = new AtomicLong();
 
+
+    private final AtomicLong couter = new AtomicLong();
     // Cria logger para informação dessa clase
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+
+        person.setId(couter.incrementAndGet());
+        person.setFirstname("Firstname " + i);
+        person.setLastname("Lastname: " + i);
+        person.setAddress("Adrress " + i);
+        person.setGender("Gender " + i);
+        return person;
+    }
 
     public Person findById(String id) {
         logger.info("Find One Person");
