cab4a254ee4dd15f6fbc7ef38a29c6d8f129136d
Diego Lima <diegolima20100@gmail.com>
2025-04-30 14:37:38 -0300

    refatoração de endpoints trocando @RequestMapping por anotações específicas como @GetMapping, @PostMapping, @PutMapping e @DeleteMapping e adiciona status code correspondende para o endpoint de delete

--- src/main/java/br/LimaDevCod3r/Controllers/PersonController.java
+++ src/main/java/br/LimaDevCod3r/Controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.LimaDevCod3r.Model.Person;
 import br.LimaDevCod3r.Services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,36 +16,33 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return this.service.findAll();
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") Long id) {
         return this.service.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody() Person person) {
         return this.service.create(person);
     }
 
 
-    @RequestMapping(method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody() Person person) {
         return this.service.update(person);
     }
 
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable("id") Long id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         this.service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
