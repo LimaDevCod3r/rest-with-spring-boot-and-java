c6653d1b514ed3cfcbc763a6087b63e0cc80a9a7
Diego Lima <diegolima20100@gmail.com>
2025-05-03 09:53:34 -0300

    adiciona os testes do Mapper de Entidade DTO

---
+++ src/test/java/br/LimaDevCod3r/unittest/mapper/Mocks/MockPerson.java
@@ -0,0 +1,58 @@
+package br.LimaDevCod3r.unittest.mapper.Mocks;
+
+import br.LimaDevCod3r.Dto.PersonDTO;
+import br.LimaDevCod3r.Model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MockPerson {
+
+
+    public Person mockEntity() {
+        return mockEntity(0);
+    }
+    
+    public PersonDTO mockDTO() {
+        return mockDTO(0);
+    }
+    
+    public List<Person> mockEntityList() {
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonDTO> mockDTOList() {
+        List<PersonDTO> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockDTO(i));
+        }
+        return persons;
+    }
+    
+    public Person mockEntity(Integer number) {
+        Person person = new Person();
+        person.setAddress("Address Test" + number);
+        person.setFirstname("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLastname("Last Name Test" + number);
+        return person;
+    }
+
+    public PersonDTO mockDTO(Integer number) {
+        PersonDTO person = new PersonDTO();
+        person.setAddress("Address Test" + number);
+        person.setFirstname("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLastname("Last Name Test" + number);
+        return person;
+    }
+
+}
