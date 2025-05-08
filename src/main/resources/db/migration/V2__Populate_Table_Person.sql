6673793f3c04cd982ffb547c8357e811d2b7e609
Diego Lima <diegolima20100@gmail.com>
2025-05-08 07:48:13 -0300

    Adiciona Flyway e cria migration para tabela 'person' com dados iniciais

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(4, 'Rua A', 'Diego', 'Male', 'Gonçalves'),
+	(5, 'Rua A', 'Diego', 'Male', 'Gonçalves'),
+	(6, 'Rua A', 'Leandro', 'Male', 'Gonçalves'),
+	(7, 'Rua A', 'Nelson', 'Male', 'Mandela'),
+	(8, 'Rua A', 'Nelson', 'Male', 'Mandela'),
+	(9, 'Rua A', 'Luciana', 'Male', 'Gonçalves');
