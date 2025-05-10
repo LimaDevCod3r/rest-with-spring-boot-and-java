d175f70df698746647035c778b2830d7c232f0a7
Diego Lima <diegolima20100@gmail.com>
2025-05-10 07:05:15 -0300

    implementando Content Negotiation via Query Parameter

--- src/main/java/br/LimaDevCod3r/Controllers/PersonController.java
+++ src/main/java/br/LimaDevCod3r/Controllers/PersonController.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,37 +17,32 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public List<PersonDTO> findAll() {
         return this.service.findAll();
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces ={ MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public PersonDTO findById(@PathVariable("id") Long id) {
         var person = this.service.findById(id);
-        person.setBirthday(new Date());
-        //person.setPhoneNumber("+55 (21) 99023-8874");
-//        person.setPhoneNumber("");
-//        person.setLastname(null);
-        person.setSensitiveData("foo bar");
         return person;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public PersonDTO create(@RequestBody() PersonDTO person) {
         return this.service.create(person);
     }
 
     @PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public PersonDTOV2 createV2(@RequestBody() PersonDTOV2 person) {
         return this.service.createV2(person);
     }
 
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public PersonDTO update(@RequestBody() PersonDTO person) {
         return this.service.update(person);
     }
