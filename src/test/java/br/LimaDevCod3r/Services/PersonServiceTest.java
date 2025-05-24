1bc5503dd6d97206677951fe3040ae117875293b
Diego Lima <diegolima20100@gmail.com>
2025-05-24 15:15:30 -0300

    Adiciona teste para o metodo de findAll com Mockito e JUnit5

--- src/test/java/br/LimaDevCod3r/Services/PersonServiceTest.java
+++ src/test/java/br/LimaDevCod3r/Services/PersonServiceTest.java
@@ -18,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -41,6 +42,123 @@ class PersonServiceTest {
 
     @Test
     void findAll() {
+        List<Person> list = input.mockEntityList();
+        when(repository.findAll()).thenReturn(list);
+        List<PersonDTO> people = service.findAll();
+
+        assertNotNull(people);
+        assertEquals(14, people.size());
+
+        var personOne = people.get(1);
+
+        personOne.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("self")
+                        && link.getHref().endsWith("/person/1")
+                        && link.getType().equals("GET")
+                );
+
+        personOne.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("findAll")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("GET")
+                );
+
+        personOne.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("create")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("POST")
+                );
+
+        personOne.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("update")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("PUT")
+                );
+
+        personOne.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("delete")
+                        && link.getHref().endsWith("/person/1")
+                        && link.getType().equals("DELETE")
+                );
+
+        assertEquals("Address Test1", personOne.getAddress());
+        assertEquals("First Name Test1", personOne.getFirstname());
+        assertEquals("Last Name Test1", personOne.getLastname());
+        assertEquals("Female", personOne.getGender());
+
+        var personFour = people.get(4);
+
+        personFour.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("self")
+                        && link.getHref().endsWith("/person/4")
+                        && link.getType().equals("GET")
+                );
+
+        personFour.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("findAll")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("GET")
+                );
+
+        personFour.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("create")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("POST")
+                );
+
+        personFour.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("update")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("PUT")
+                );
+
+        personFour.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("delete")
+                        && link.getHref().endsWith("/person/4")
+                        && link.getType().equals("DELETE")
+                );
+
+        assertEquals("Address Test4", personFour.getAddress());
+        assertEquals("First Name Test4", personFour.getFirstname());
+        assertEquals("Last Name Test4", personFour.getLastname());
+        assertEquals("Male", personFour.getGender());
+
+        var personSeven = people.get(7);
+
+        personSeven.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("self")
+                        && link.getHref().endsWith("/person/7")
+                        && link.getType().equals("GET")
+                );
+
+        personSeven.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("findAll")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("GET")
+                );
+
+        personSeven.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("create")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("POST")
+                );
+
+        personSeven.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("update")
+                        && link.getHref().endsWith("/person")
+                        && link.getType().equals("PUT")
+                );
+
+        personSeven.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("delete")
+                        && link.getHref().endsWith("/person/7")
+                        && link.getType().equals("DELETE")
+                );
+
+        assertEquals("Address Test7", personSeven.getAddress());
+        assertEquals("First Name Test7", personSeven.getFirstname());
+        assertEquals("Last Name Test7", personSeven.getLastname());
+        assertEquals("Female", personSeven.getGender());
     }
 
 
